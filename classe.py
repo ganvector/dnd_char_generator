d429c48fd2ac1992ecb6d17388b3d51bb7963cba
milliardo <vitor.gan21@gmail.com>
2018-05-26 17:54:52 -0300

    atualizando hitdice das classes

--- classe.py
+++ classe.py
@@ -10,64 +10,64 @@ class Bard:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 8
 
 class Warlock:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 8
 
 class Cleric:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 8
 
 class Druid:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 8
 
 class Sorcerer:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 6
 
 class Fighter:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 10
 
 class Rogue:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 8
 
 class Mage:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 6
 
 class Monge:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 8
 
 class Paladin:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 10
 
 class Ranger:
 
     def __init__(self, level):
         self.level = level
-        self.hitdice = 12
+        self.hitdice = 10
