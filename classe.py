3915b19c6267894d218e1b8c09c99cf9946c62b4
milliardo <vitor.gan21@gmail.com>
2018-05-26 17:44:21 -0300

    terminado base das classes

--- classe.py
+++ classe.py
@@ -5,3 +5,69 @@ class Barbarian:
     def __init__(self, level):
         self.level = level
         self.hitdice = 12
+
+class Bard:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Warlock:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Cleric:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Druid:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Sorcerer:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Fighter:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Rogue:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Mage:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Monge:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Paladin:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
+
+class Ranger:
+
+    def __init__(self, level):
+        self.level = level
+        self.hitdice = 12
